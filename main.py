0c661f6b3126881dd9be7109b99a3dbb706d6202
Thomas Trimbur <spidy212@msn.com>
2022-12-27 03:09:44 -0500

    Added dice game functionality

--- main.py
+++ main.py
@@ -1,29 +1,38 @@
 import getopt
+import pickle
 import random
 import sys
 import time
-import checks
+from decimal import Decimal
 
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
-from decimal import Decimal
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+
+import checks
 
 #################[GLOBAL VARIABLES]#################
 SIM_FLAG = False
-RAND_CHOICE = False
-DEFAULT_COLOUR = 'r'
 balance = Decimal('9999999.99')
 USERNAME = ''
 PASS = ''
 LOSS_THRESHOLD = 5
 default_bet = Decimal('0.5')
+gamble_type = "dice"
+cookies = None
 #################[GLOBAL VARIABLES]#################
-
+################[ROULETTE DEFAULTS]#################
+RAND_CHOICE = False
+DEFAULT_COLOUR = 'r'
+################[ROULETTE DEFAULTS]#################
+##################[DICE DEFAULTS]###################
+roll_ratio = 47.5
+dice_wait = 3
+##################[DICE DEFAULTS]###################
 
 def readArgs():
     global USERNAME, PASS, SIM_FLAG, LOSS_THRESHOLD, RAND_CHOICE, balance, default_bet
@@ -64,6 +73,7 @@ def readArgs():
 
 
 def login():
+    global cookies
     # Click the login button
     WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, '/html/body/cw-root/cw-header/nav/div[2]/div/cw-auth-buttons/div/button'))).click()
     # Find and fill username box
@@ -82,14 +92,107 @@ def login():
     auth_box = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[4]/div[3]/div/div/div/form/div[3]/div[1]/div/input')))
     auth_box.clear()
     auth_box.send_keys(auth_code)
-    # CLik the submit button
+    # Click the submit button
     WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[4]/div[3]/div/div/div/form/div[4]/div[1]/div[1]'))).click()
-    # Reset bet amount
-    bet_box = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, '/html/body/cw-root/mat-sidenav-container/mat-sidenav-content/div/cw-roulette/div/div[2]/cw-amount-input/mat-form-field/div/div[1]/div[3]/div[2]/input')))
-    bet_box.clear()
 
+    #Wait until we are returned to CS:GO Roll
+    time.sleep(3)
 
-def gamble():
+    #Grab session cookies to bring to dice page
+    pickle.dump(driver.get_cookies(), open("cookies.pkl","wb"))
+
+    if gamble_type == "dice":
+        driver.get('https://www.csgoroll.com/en/dice')
+        cookies = pickle.load(open("cookies.pkl", "rb"))
+        for cookie in cookies:
+            driver.add_cookie(cookie)
+
+def gamble_dice():
+    global balance, DEFAULT_COLOUR
+    total_spins = 0
+    losses = 0
+    earnings = Decimal('0.0')
+    total_earnings = Decimal('0.0')
+    last_bet = Decimal('0.0')
+    won_last_round = True
+    highest_best = default_bet
+
+    while True:
+        # Do Checks
+        if checks.check_losses(losses, LOSS_THRESHOLD):
+            losses = 0
+            last_bet = default_bet
+        elif checks.check_earnings(earnings, default_bet):
+            total_earnings = total_earnings + earnings
+            earnings = 0
+        elif checks.check_spins(total_spins):
+            sys.exit(1)
+
+        # Set bet amount
+        bet_amount_element = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, '/html/body/cw-root/mat-sidenav-container/mat-sidenav-content/div/cw-dice/section[1]/cw-dice-wager/section[3]/form/div/section[1]/div[1]/cw-amount-input/mat-form-field/div/div[1]/div[3]/div[1]/input')))
+        bet_amount_element.clear()
+        if won_last_round:
+            bet_amount_element.send_keys(str(default_bet))
+            last_bet = Decimal(str(default_bet))
+        else:
+            bet = str(last_bet * Decimal('2.0'))
+            bet_amount_element.send_keys(bet)
+            last_bet = Decimal(bet)
+        
+        # Set Roll Ratio Amount
+        roll_under_ratio_element = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, '/html/body/cw-root/mat-sidenav-container/mat-sidenav-content/div/cw-dice/section[1]/cw-dice-wager/section[3]/form/div/div[1]/div[1]/div/mat-form-field/div/div[1]/div[3]/input')))
+        roll_under_ratio_element.clear()
+        roll_under_ratio_element.send_keys(str(roll_ratio))
+
+        # Press green bet button
+        button = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, '/html/body/cw-root/mat-sidenav-container/mat-sidenav-content/div/cw-dice/section[1]/cw-dice-wager/section[3]/form/div/section[2]/div[2]/button')))
+
+        if not SIM_FLAG:
+            button.click()
+
+        if last_bet > highest_best:
+            highest_best = last_bet
+        
+        total_spins += 1
+        print('[Round {}] Dice Roll was created, placed bet of {:.2f} with a ratio of {}'.format(total_spins, last_bet, roll_ratio))
+
+        # Get wager result
+        wager_response = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "/html/body/cw-root/mat-sidenav-container/mat-sidenav-content/div/cw-dice/section[1]/cw-dice-wager/section[3]/form/div/div[3]/cw-pretty-balance"))
+        )
+        
+        while True:
+            class_name = wager_response.get_attribute('class')
+            if 'text-success' in class_name:
+                print('\tWinner!\t')
+                won_last_round = True
+                losses = 0
+                earnings = earnings + last_bet
+                balance += last_bet
+            elif 'text-danger' in class_name:
+                print('\tLoss\t')
+                won_last_round = False
+                losses += 1
+                earnings = earnings - last_bet
+                balance -= last_bet
+            else:
+                pass
+
+            print('\tEarnings = {:.2f}\n\tBalance = {:.2f}\n\tTotal Earnings = {:.2f}'.format(earnings, balance, total_earnings))
+            
+            if not SIM_FLAG:
+                button.click()
+
+            print(f"Completed spin... waiting {dice_wait} seconds")
+            
+            time.sleep(dice_wait)
+
+            with open('spin_log.txt', 'a+') as file:
+                file.write('[Round {}/{} - {}]: Highest Bet = {:.2f}/{:.2f}  -  Total Earnings = {:.2f}  -  Balance = {:.2f}\n'.format(total_spins, DEFAULT_COLOUR, won_last_round, highest_best, last_bet, total_earnings, balance))
+                file.close()
+            break
+
+def gamble_wheel():
     global balance, DEFAULT_COLOUR
     total_spins = 0
     total_blacks = 0
@@ -102,6 +205,10 @@ def gamble():
     last_bet = Decimal('0.0')
     won_last_round = True
     highest_best = default_bet
+
+    # Reset bet amount
+    bet_box = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, '/html/body/cw-root/mat-sidenav-container/mat-sidenav-content/div/cw-roulette/div/div[2]/cw-amount-input/mat-form-field/div/div[1]/div[3]/div[2]/input')))
+    bet_box.clear()
 
     while True:
         # Do Checks
@@ -135,7 +242,6 @@ def gamble():
 
             # Press red bet button
             button = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, '/html/body/cw-root/mat-sidenav-container/mat-sidenav-content/div/cw-roulette/div/div[2]/div[2]/section/cw-roulette-bet-button[1]/div/button')))
-            DEFAULT_COLOUR = 'r'
             if RAND_CHOICE:
                 DEFAULT_COLOUR = random.choice(['r', 'b', 'g'])
                 if DEFAULT_COLOUR == 'g':
@@ -233,6 +339,9 @@ try:
     readArgs()
     login()
     time.sleep(10)
-    gamble()
+    if gamble_type == "wheel":
+        gamble_wheel()
+    else:
+        gamble_dice()
 finally:
     driver.quit()
